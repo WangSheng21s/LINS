f68b0e58739769b4c383db039239c763377bba35
WangSheng21s <102408340+WangSheng21s@users.noreply.github.com>
2024-07-09 22:47:09 +0800

    Update modeling_medlinker.py

--- model/modeling_medlinker.py
+++ model/modeling_medlinker.py
@@ -70,7 +70,6 @@ Question_Decomposition_prompt = """
 
 
 Passage_Coherence_prompt = """
-You are an expert in determining whether there is consistency between the generated sentence and the retrieved paragraph. If there is a content conflict between the generated sentence and the retrieved paragraph, you would answer 'conflict', if the content of the two is consistent, you would answer 'coherence', and if the content of the two is not related, you would answer 'irrelevant'. Be careful to get to the point when answering, don't have any explanations or chitchat, just answer "conflict" or "coherence" or "irrelevant".
 
 """
 
@@ -159,43 +158,6 @@ class MedLinker:
         prompt = Passage_Coherence_prompt + "sentence: " + sentence + "\npassage: " + passage + "\nanswer:"
         response, history = self.chat(tokenizer=self.tokenizer, prompt=prompt, history=None)
         return response
-
-
-    @torch.no_grad()
-    def query(self, question, refs ="", filter_with_different_urls=True, with_omim=False, topk=5):
-        references_str = ''
-        if refs == "":
-            refs = self.ref_retriever.medlinker_merage(query=question, filter_with_different_urls=filter_with_different_urls, topk=topk, with_omim=with_omim)
-            if not refs:
-                return { "references": [], "answer": "" }
-            for ix, ref in enumerate(refs["texts"]):
-                txt = ref
-                references_str += f'[{ix+1}]: {txt}' '\\'
-        else:
-            for ix, ref in enumerate(refs["texts"]):
-                references_str += f'[{ix+1}]: {ref}' '\\'
-
-        #prompt = 'There is now the following question:\n ' + question + '\n The following references have been collected from the internet so far:\n ' + '[' + prompt + ']' + '\n Please refer to the above references and give a valid, reasonable, complete and logical answer, with the requirement to give the appropriate citations.'
-        #sub_add = "Here is an example: There have some references:\n [[1]:'Some lessons from Vietnam were clear: America fails when it attempts military action without a clear and achievable strategic goal, and when it fails to adequately understand the ideology of the enemy.', [2]:'https://www.nytimes.com/1973/04/01/archives/have-we-learned-or-only-failed-the-lessons-of-vietnam-vietnam.html', [3]:'For instance, one of the primary American history textbooks now used in the Garden Grove Unified School District includes more than 22 pages on the Vietnam War. It also asks students to respond to such questions as, “In what ways did U.S. military planners fail to understand the Vietnamese culture?” and, “The domino theory is still used by some politicians to explain global politics. Do you agree with the theory?”', [4]:'Once too controversial to be taught in American high schools, the Vietnam War has slowly slipped into the curriculum, but only in a superficial way, educators say. Although many teachers consider Vietnam crucial to understanding American foreign policy and identify it as one of the key events in U.S. history, today’s high school students learn about the Vietnam War in much the same way they study other contemporary affairs--quickly and without much depth, educators say.',[5]: '“I think it’s a shame that high school students don’t learn more about this war, because the Vietnam War is still constantly referred to in public discourse as a standard to measure proposed military intervention,” said Jerry Starr, director of the Center for Social Studies Education in Pittsburgh, Pa., who has done extensive research on how the Vietnam War is taught in American schools.']\n\n Please refer to the above references and give a valid, reasonable, complete and logical answer, with the requirement to give the appropriate citations.\nQuestion：Why did the US fail in Vietnam and why is never really taught in schools?\nAnswer：The US failed in Vietnam because it attempted military action without a clear and achievable strategic goal, and failed to adequately understand the ideology of the enemy[1]. It is rarely taught in schools because the Vietnam War has slowly slipped into the curriculum, but only in a superficial way[4][5]. The primary American history textbooks now used in the Garden Grove Unified School District include some information on the Vietnam War, but it is usually not taught in depth[3]. According to Jerry Starr, director of the Center for Social Studies Education in Pittsburgh, Pa., the Vietnam War is still constantly referred to in public discourse as a standard to measure proposed military intervention, but high school students don't learn enough about the war[5].\n"
-        prompt = "Answer the following question about medical as best you can. There have some references:\n" + references_str + "\n Please refer to the above references and give a valid, reasonable, complete and logical answer, with the requirement to give the appropriate citations.\n" + f"Question: {question}"
-        inputs = self.tokenizer(prompt, return_tensors="pt")
-        inputs = inputs.to(self.device)
-        pred = self.model.generate(**inputs, max_new_tokens=512, top_k=0, top_p=0.8,chat_format="raw",
-                                    repetition_penalty=1.2,
-                                    eos_token_id=151643,
-                                    pad_token_id= 151643,
-                                    stop_words_ids= [[151643]],
-                                    do_sample= True,
-                                    transformers_version= "4.31.0")
-        response = self.tokenizer.decode(pred.cpu()[0], skip_special_tokens=True)
-        #resopnse去掉prompt
-        response = response.replace(prompt, "")
-        answer = response.split("\n\n")
-        for i in range(len(answer)):
-            if len(answer[i]) > 100:
-                response = answer[i]
-                break
-        return { "answer": response, "references": refs}
     
 
     @torch.no_grad()
@@ -251,40 +213,7 @@ class MedLinker:
                 continue
         return literature_info
 
-    @torch.no_grad()
-    def Original_RAG(self, question, filter_with_different_urls=False, topk=5, if_pubmed=True, if_short_sentences=False, local_data_name="")
-        search_results = self.keyword_search(retriever_query, topk=5, if_short_sentences=if_short_sentences)
-        if search_results == None:
-            return { "references": [], "answer": "" }
-    @torch.no_grad()
-    def keywords_query_chat(self, question, refs = "", filter_with_different_urls=False, with_omim=False, topk=5, if_pubmed = True, if_merge=False, if_short_sentences = True):
-        references_str = ''
-        retriever_query = question.split("options")[0]
-        if refs == "":
-            search_results = self.keyword_search(retriever_query, topk=5, if_short_sentences=if_short_sentences)
-            if search_results == None:
-                return { "references": [], "answer": "" }
-            recall_search_results = self.ref_retriever.medlinker_query(question=retriever_query, data_list=search_results, filter_with_different_urls=False)
-            rerank_search_results = self.ref_retriever.medlinker_rerank(query=retriever_query, search_results=recall_search_results)
-            merge_search_results = self.ref_retriever.medlinker_merage(query=retriever_query, search_results=rerank_search_results, if_merge=if_merge, topk=topk, if_pubmed=if_pubmed, local_data_name="", filter_with_different_urls=False)
-            refs = merge_search_results
-
-            if not refs:
-                return { "references": [], "answer": "" }
-            for ix, ref in enumerate(refs["texts"]):
-                txt = ref
-                references_str += "[" + str(ix+1) + "] " + txt + " \n"
-        else:
-            for ix, ref in enumerate(refs["texts"]):
-                references_str += "[" + str(ix+1) + "] " + ref + " \n"
         
-        prompt = "MedLinker:" + "question:" + question + "\nretrieval_texts:" + references_str
-        response, history = self.chat(tokenizer=self.tokenizer, prompt=prompt, history=None)
-        return { "answer": response, "references": refs}
-
-
-        
-            
 
     @torch.no_grad()
     def Original_RAG(self, question, filter_with_different_urls=False, topk=5, if_pubmed=True, if_merge=False, if_short_sentences=False, local_data_name=""):
@@ -294,10 +223,10 @@ class MedLinker:
             if search_results != None and search_results != []:
                 recall_search_results = self.ref_retriever.medlinker_query(question=question, data_list=search_results, filter_with_different_urls=filter_with_different_urls)
                 rerank_search_results = self.ref_retriever.medlinker_rerank(query=question, search_results=recall_search_results)
-                merge_search_results = self.ref_retriever.medlinker_merage(query=question, search_results=rerank_search_results, if_merge=if_merge, topk=topk, if_pubmed=if_pubmed, local_data_name="", filter_with_different_urls=filter_with_different_urls)
+                merge_search_results = self.ref_retriever.medlinker_merage(query=question, search_results=rerank_search_results, topk=topk, if_pubmed=if_pubmed, local_data_name="", filter_with_different_urls=filter_with_different_urls)
                 refs = merge_search_results
         else:
-            refs = self.ref_retriever.medlinker_merage(query=question, filter_with_different_urls=False, topk=topk, if_pubmed=False, if_merge=False, local_data_name=local_data_name)
+            refs = self.ref_retriever.medlinker_merage(query=question, filter_with_different_urls=False, topk=topk, if_pubmed=False, local_data_name=local_data_name)
         if refs != None and refs != []:
             for ix, ref in enumerate(refs["texts"]):
                 references_str += "[" + str(ix+1) + "] " + ref + " \n"
